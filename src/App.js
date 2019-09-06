3690615883c4c4bc8a87f35bab87f6ca164dc9aa
LauriKukko <lauri.kukkonen@gmail.com>
2019-09-06 15:43:14 +0300

    Eka kommitti. Peli luotu. Pientä säätöä jäljellä.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import Jumbotron from "react-bootstrap/Jumbotron";
 import './App.css';
+import Peli from "./Peli";
+import logo from './AWorkXkirkas.png';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Peli/>
+      <Jumbotron className="Jumbotron">
+        <h1 className="jumbootsikko">Academykonsulttien levinneisyyspeli</h1>
+          <h2>Tervetuloa kokeilemaan miten kauan konsulttisi selviävät urallaan.</h2>
+          <img src={logo} className="Logo"/>
+
+          <p>
+              Tervetuloa pelaamaan! Sinun roolisi tässä pelissä on tuoda uusia konsultteja urakentälle.
+              Jotta konsultit jatkaisivat uraansa mahdollisimman pitkään, onsinun luotava heille mahdollisuudet menestyä.
+              Tämä toteutuu luomalla uusille konsulteillesi parhaat mahdolliset verkostot. Verkostoissa ei voi olla liikaa ihmisiä, koska tällöin piirit kääntyvät sisäänpäin ja näivettyvät.
+              Toisaalta jos jätät konsulttisi yksin kentälle, he kokevat olonsa uhatuksi ja lähtevät takaisin omiin vanhoihin ammatteihinsa.
+              Klikkaile ruudukosta konsulteillesi sopivat positiot ja katso miten Academyn ilosanoma leviää <br/><br/>- Jos leviää.
+              <br/> Jos konsulttiesi urat jäävät polkemaan paikalleen, lähetä lisää porukkaa auttamaan heidät uudelle kiihdytyskaistalle.
+
+        Tämä peli on toteutettu vanhalla "the Game of Life"-tyylillä.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Jumbotron>
+
     </div>
   );
 }
